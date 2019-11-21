883b47b89f22728c78e7665e2ae33b798d75c0ed
Maynic Ye <zye03@ucsb.edu>
2019-11-20 22:13:48 -0800

    my - rm JSON Result, type in metadata, page2, mv "title" to left-most, combine first two tables

--- src/main/java/hello/geojson/Properties.java
+++ src/main/java/hello/geojson/Properties.java
@@ -3,7 +3,7 @@ package hello.geojson;
 public class Properties {
   public double mag;
   public String place;
-  public String type;
+//   public String type;
   public String title;
   public String url;
 }
