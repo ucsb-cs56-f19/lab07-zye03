3897619c3bfd0f8e4de6621677816f8209abdde7
Maynic Ye <zye03@ucsb.edu>
2019-11-20 21:34:08 -0800

    my - add id, place, mag to result and make them work

--- src/main/java/hello/geojson/FeatureCollection.java
+++ src/main/java/hello/geojson/FeatureCollection.java
@@ -7,9 +7,12 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+
 public class FeatureCollection {
     private static Logger logger = LoggerFactory.getLogger(FeatureCollection.class);
     public Metadata metadata;
+    public List<Feature> features;
     public String type;
 
     /**
